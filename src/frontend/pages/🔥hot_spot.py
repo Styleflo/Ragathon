89350c920c83371c77bccd9abd95dba5540c2288
Marin Boehm <marin.boehm@etu.univ-tours.fr>
2026-02-26 07:58:00 -0500

    regroup hotspot, trends and signal into synthesis

--- src/frontend/pages/🔥hot_spot.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Hot Spot - Montréal", page_icon="🔥", layout="wide")
-st.title("🔥 Hot Spot - Montréal")
-
-response_container = st.container()
-
-with response_container:
-    st.markdown("Contenu à venir...")
