2c5568759b3b023e7efa83b0d6592fe39ff4e4b3
Florian Touraine <128731709+Styleflo@users.noreply.github.com>
2026-02-25 20:57:32 -0500

    ajout graphique 1

--- src/frontend/pages/🗺️dashboard.py
+++ src/frontend/pages/🗺️dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import date
 import streamlit.components.v1 as components
+from services.graph import heatmap_collisions_filtree
 
 st.set_page_config(page_title=" Dashboard - Montréal", page_icon="🗺️", layout="wide")
 st.title("🗺️ Dashboard - Montréal")
@@ -8,16 +9,16 @@ st.title("🗺️ Dashboard - Montréal")
 with st.sidebar:
     st.header("Période")
 
-    date_debut = st.date_input("Date de début", value=date(2026, 1, 1))
-    date_fin = st.date_input("Date de fin", value=date.today())
+    date_debut = str(st.date_input("Date de début", value=date(2015, 1, 1)))
+    date_fin = str(st.date_input("Date de fin", value=date.today()))
 
 
     # Remplacer par les fonctions qui génèrent les graphiques en fonction des dates sélectionnées
     if "html_code_1" not in st.session_state:
-        st.session_state.html_code_1 = """ fonction(date_debut, date_fin) """
-        st.session_state.html_code_2 = """ fonction(date_debut, date_fin) """
-        st.session_state.html_code_3 = """ fonction(date_debut, date_fin) """
-        st.session_state.html_code_4 = """ fonction(date_debut, date_fin) """
+        st.session_state.html_code_1 = heatmap_collisions_filtree(date_debut, date_fin)
+        st.session_state.html_code_2 = heatmap_collisions_filtree(date_debut, date_fin)
+        st.session_state.html_code_3 = heatmap_collisions_filtree(date_debut, date_fin)
+        st.session_state.html_code_4 = heatmap_collisions_filtree(date_debut, date_fin)
 
     is_valid_date = date_debut > date_fin
     if is_valid_date:
@@ -28,33 +29,33 @@ with st.sidebar:
         st.session_state.date_fin = date_fin
 
         # Remplacer par les fonctions qui génèrent les graphiques en fonction des dates sélectionnées
-        st.session_state.html_code_1 = """ fonction(date_debut, date_fin) """
-        st.session_state.html_code_2 = """ fonction(date_debut, date_fin) """
-        st.session_state.html_code_3 = """ fonction(date_debut, date_fin) """
-        st.session_state.html_code_4 = """ fonction(date_debut, date_fin) """
+        st.session_state.html_code_1 = heatmap_collisions_filtree(date_debut, date_fin)
+        st.session_state.html_code_2 = heatmap_collisions_filtree(date_debut, date_fin)
+        st.session_state.html_code_3 = heatmap_collisions_filtree(date_debut, date_fin)
+        st.session_state.html_code_4 = heatmap_collisions_filtree(date_debut, date_fin)
 
         st.rerun()
 
-col1, col2 = st.columns(2)
+# col1, col2 = st.columns(2)
 
-with col1:
-    with st.container():
-        st.subheader("Heatmap des collisions")  
-        components.html(st.session_state.html_code_1, height=200)  
+# with col1:
+with st.container():
+    st.subheader("Heatmap des collisions")
+    components.html(st.session_state.html_code_1, height=500)
 
-with col2:
-    with st.container():
-        st.subheader("Graphique 2") #Titre a modifier
-        components.html(st.session_state.html_code_2, height=200)
+# with col2:
+with st.container():
+    st.subheader("Graphique 2") #Titre a modifier
+    components.html(st.session_state.html_code_2, height=500)
 
-col3, col4 = st.columns(2)
+# col3, col4 = st.columns(2)
 
-with col3:
-    with st.container():
-        st.subheader("Graphique 3") #Titre a modifier
-        components.html(st.session_state.html_code_3, height=200)
+# with col3:
+with st.container():
+    st.subheader("Graphique 3") #Titre a modifier
+    components.html(st.session_state.html_code_3, height=500)
 
-with col4:
-    with st.container():
-        st.subheader("Graphique 4") #Titre a modifier
-        components.html(st.session_state.html_code_4, height=200)
+# with col4:
+with st.container():
+    st.subheader("Graphique 4") #Titre a modifier
+    components.html(st.session_state.html_code_4, height=500)
